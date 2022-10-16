a1672a64ab58be9b147485946abbd6c0a3a5f8b0
Hammad Ali <hammadd2@yahoo.com>
2022-10-16 22:09:54 +0500

    Third and forth assignments completed, but 3rd titlecase assignment still needs to solve

---
+++ 3-Namecases/index.js
@@ -0,0 +1,5 @@
+
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+var personName="Eric"
+personName.toLowerCase();
+personName.toUpperCase();
---
+++ 4-Famous Quote/index.js
@@ -0,0 +1,3 @@
+
+var str= "Albert Einstein once said,"+ "\"A person who never made a mistake never tried anything new.” \n"
+console.log(str)
