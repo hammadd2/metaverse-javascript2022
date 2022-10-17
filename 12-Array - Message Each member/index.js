9530e181a245f80403df195bc4567e1583034217
Hammad Ali <hammadd2@yahoo.com>
2022-10-17 16:45:52 +0500

    Assignment

---
+++ 12-Array - Message Each member/index.js
@@ -0,0 +1,12 @@
+var myFriends=["Abid","Ahsan","Tariq","Umair","Tauseef", "Atif"];
+//Message to All members
+alert(myFriends);
+
+
+//Message to Each friend's name
+alert("Hello " + myFriends[0]);
+alert("Hello " + myFriends[1]);
+alert("Hello " + myFriends[2]);
+alert("Hello " + myFriends[3]);
+alert("Hello " + myFriends[4]);
+alert("Hello " + myFriends[5]);
---
+++ 13-Array -Transport list/index.js
@@ -0,0 +1,5 @@
+var myProducts=["Honda Motorcycle","Rado Watch","Mult Family Home","DHA Property"];
+
+//MPrint all products
+console.log("I would like to own a "+ myProducts[0]  + ' ,' + myProducts[1] + ' ,' + myProducts[2] + ' and ' + myProducts[3]);
+
