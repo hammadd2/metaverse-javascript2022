7458ddffa29b4bf750dc1f7ce4f1bb34611173cd
Hammad Ali <hammadd2@yahoo.com>
2022-10-17 17:01:50 +0500

    Assignment

---
+++ 14-Array - Invitation Message/index.js
@@ -0,0 +1,10 @@
+var myGuests=["Tauseef","Ahsan","Tariq","Umair", "Atif"];
+//Message to All members
+
+
+//Message to Each friend's name
+alert("Hello Dear " + myGuests[0] + ", Dr. Zia Khan invites You for the Dinner party at KFC");
+alert("Hello Dear " + myGuests[1]+ " , Dr. Zia Khan invites You for the Dinner party at KFC");
+alert("Hello Dear " + myGuests[2]+ " , Dr. Zia Khan invites You for the Dinner party at KFC");
+alert("Hello Dear " + myGuests[3]+ " , Dr. Zia Khan invites You for the Dinner party at KFC");
+alert("Hello Dear " + myGuests[4]+ " , Dr. Zia Khan invites You for the Dinner party at KFC");
